3517072c29acf03fbc4aa67fd4adfc28386b440c
shrasht971 <94127570+shrasht971@users.noreply.github.com>
2025-06-03 18:31:28 +0530

    Create program Volume of shpere

---
+++ Basic Questions/Vol_Sphere.java
@@ -0,0 +1,9 @@
+public class Vol_Sphere {
+
+    public static void main(String[] args) {
+        int r = 4;
+        double pi = 3.14;
+        double volume = (4.0f / 3.0f) * pi * r * r * r; 
+        System.out.println("Volume of the sphere: " + volume);
+    }
+}
