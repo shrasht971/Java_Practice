e2d94bc88d0006cceec1f9b37ef8317f788681f7
shrasht971 <94127570+shrasht971@users.noreply.github.com>
2026-05-09 01:53:04 +0530

    write a program for reverse right half pyramid used for loop

---
+++ pattern/Reverse_Right_Half_Pyramid.java
@@ -0,0 +1,13 @@
+package pattern;
+
+public class Reverse_Right_Half_Pyramid {
+    public static void main(String[] args){
+       int n=5;
+       for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i+1;j++){
+        System.out.print("* ");
+       }
+       System.out.println(" ");
+       }
+    }
+}
