58522c53075fb93cb2c1c71962c518be5ed595db
shrasht971 <94127570+shrasht971@users.noreply.github.com>
2026-05-10 11:01:29 +0530

    Write a program for Number increase make right  pyramid use for loop

---
+++ pattern/Number_Increase_Pyramid.java
@@ -0,0 +1,12 @@
+package pattern;
+
+public class Number_Increase_Pyramid {
+    public static void main(String[] args){
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=i;j++){
+       System.out.print(j);
+    }
+    System.out.println(" ");
+}
+}
+}
