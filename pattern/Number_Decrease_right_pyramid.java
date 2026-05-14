0ed9b7bd60ddbb280394252be98ba203b78f9467
shrasht971 <94127570+shrasht971@users.noreply.github.com>
2026-05-15 00:52:53 +0530

    Write a program for number increasing reverse pyramid

---
+++ pattern/Number_Decrease_right_pyramid.java
@@ -0,0 +1,12 @@
+package pattern;
+
+public class Number_Decrease_right_pyramid {
+    public static void main(String[] args){
+       for(int i=1;i<=5;i++){
+        for(int j=1;j<=5-i;j++){
+        System.out.print(j);
+       }
+       System.out.println();
+    }
+    }
+}
